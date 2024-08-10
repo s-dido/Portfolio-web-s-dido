9f0768d6d515f12ee6b1d3c3ce03fad17de66997
s-dido <didomenicosantino29l@gmail.com>
2024-08-10 13:13:42 -0300

    debuggin scene render

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,9 @@
 import {Suspense, useEffect, useState} from 'react';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, extend } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { Loader} from '../Loader';
 
-import CanvasLoader from '../Loader'
+
 
 
 // [] ``
@@ -12,17 +13,18 @@ const Computers = () => {
   
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor={"black"}/>
+      <hemisphereLight intensity={4} groundColor={"black"}/>
       <pointLight intensity={1}/>
-      <primitive object={scene}/>
+      <spotLight position={[-20, 50 ,10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize ={1024} />
+      <primitive object={scene} scale ={0.60} position ={[0, -4, -1.5 ]} rotation = {[-0.01, -0.2, -0.1]}/>
     </mesh>
   )
 }
 
 const ComputersCanvas = () =>{
   return(
-    <Canvas frameloop='demand' shadows camera = {{position: [20, 5, 10], fov: 25}} gl={{preserveDrawingBuffer: true}}>
-        <Suspense fallback={<CanvasLoader/>}>
+    <Canvas frameloop= "demand" shadows camera = {{position: [20, 3, 5], fov: 25}} gl={{preserveDrawingBuffer: true}}>
+        <Suspense fallback={<Loader/>}>
           <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2}/>
         <Computers/>
         </Suspense>
